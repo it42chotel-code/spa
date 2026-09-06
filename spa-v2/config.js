bd32cc548f23516ab4a3943bfb483b2968c949eb
zoulkinx-demo <master.zoulkinx@gmail.com>
2026-09-07 01:38:13 +0700

    config: connect Spa Queue V2 to live Apps Script API

--- spa-v2/config.js
+++ spa-v2/config.js
@@ -3,7 +3,7 @@
  * Example: https://script.google.com/macros/s/DEPLOYMENT_ID/exec
  */
 window.SPA_DISPLAY_CONFIG = Object.freeze({
-  apiUrl: '',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbzCAXlG4B5MajrHBRjuccRhGOKthWO6jbMzK3MLXFnKyw7AlZWVRyyklDGt-MSvVH2n/exec',
   refreshMs: 10000,
   staleAfterMs: 35000,
   criticalAfterMs: 120000,
